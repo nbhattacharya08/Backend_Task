9b90f3b82b8aced878e44475d7149d6ce80bad61
Nilanjan Bhattacharya <nilanjanbhattacharya22@gmail.com>
2025-02-17 14:16:40 +0530

    fix: Basic CRUD routes added to the server

--- Routes/index.routes.js
+++ Routes/index.routes.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const userRouter = require('./user.routes');
+
+router.get('/api', userRouter);
+
+module.exports = router;
--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    }
-);
+const indexRouter = require('./index.routes');
+
+app.use(express.json());
+
+app.use('/', indexRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
