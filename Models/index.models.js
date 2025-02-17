40e342cddd9f49930f815d83199e332118b1f2c5
Nilanjan Bhattacharya <nilanjanbhattacharya22@gmail.com>
2025-02-17 14:38:50 +0530

    feat: Models created

---
+++ Models/index.models.js
@@ -0,0 +1,9 @@
+const userModel = require('./user.model');
+
+const indexModels = {
+    user: userModel
+};
+
+module.exports = indexModels;
+
+
---
+++ Models/user.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define collection and schema for User
+let User = new Schema({
+    name: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    role: {
+        type: String
+    },
+});
+
+module.exports = mongoose.model('User', User);
